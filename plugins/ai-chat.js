c18f9afbff0180532139b1731a3fee0150f7bb51
beamer254 <jinwoobot254@gmail.com>
2025-04-09 00:45:18 +0300

    Update ai-chat.js

--- plugins/ai-chat.js
+++ plugins/ai-chat.js
@@ -1,7 +1,7 @@
-const { malvin } = require('../malvin');
+const { lee } = require('../lee');
 const axios = require('axios');
 
-malvin({
+lee({
     pattern: "ai",
     alias: ["bot", "dj", "gpt", "gpt4", "bing"],
     desc: "Chat with an AI model",
@@ -30,7 +30,7 @@ async (conn, mek, m, { from, args, q, reply, react }) => {
     }
 });
 
-malvin({
+lee({
     pattern: "openai",
     alias: ["chatgpt", "gpt3", "open-gpt"],
     desc: "Chat with OpenAI",
@@ -59,7 +59,7 @@ async (conn, mek, m, { from, args, q, reply, react }) => {
     }
 });
 
-malvin({
+lee({
     pattern: "deepseek",
     alias: ["deep", "seekai"],
     desc: "Chat with DeepSeek AI",
